b2040fa792977cbf40260b5c062e7c3b0ae90370
Muhammad Akhtar <45616014+Mohammad-Akhtar-Awan@users.noreply.github.com>
2023-04-01 06:48:50 +0500

    Time: 40 ms (66.84%), Space: 15.3 MB (19.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,22 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if len(s) != len(t):
+            return False
+    
         s_hash = {}
         t_hash = {}
         
-        for i in s:
-            if i in s_hash.keys():
-                s_hash[i] += 1
-            else:
-                s_hash[i] = 1
-
-        for i in t:
-            if i in t_hash.keys():
-                t_hash[i] += 1
-            else:
-                t_hash[i] = 1
-                
+        for i in range(len(s)):
+            s_hash[s[i]] = 1 + s_hash.get(s[i], 0)
+            t_hash[t[i]] = 1 + t_hash.get(t[i], 0)    
+        
         if s_hash == t_hash:
             return True
-        else:
-            return False
+        return False
